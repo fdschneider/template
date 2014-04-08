ba6c7e32e73d8f9309264d742519816ab6fb297c
Kevin Cazelles <Kevin.Cazelles@univ-montp2.fr>
2014-04-08 11:29:59 +0200

    Some idea I had

--- template.r
+++ template.r
@@ -6,16 +6,47 @@ y <- 0.5 + x*3.1 + rnorm(30, 0, 0.2)
 plot(y ~ x)
 
 
-
-# template function
+## template function
 ## for now just returning an object of class "template". 
 
-template <- function(... ) {
- out <- list(...)
-  class(out) <- "template"
- return(out)
+template <- function(journal="nature", bty="o", verbose=TRUE) {
+	
+	# Match with a vector of journal 
+ 	journals <- c("nature", "science", "ecologyletters")
+	w <- charmatch(journal,journals)
+	if (is.na(w)) stop("No match for journal found")
+	
+	# Then we create our object whode class is "template"
+	if (w==1){
+		out <- par(xaxs="i", yaxs="i", bty=bty, bg=2)
+	}
+	
+	if (w==2){
+		out <- par(xaxs="i", yaxs="i", bty=bty)
+	}
+	
+	if (w==3){
+		out <- par(xaxs="i", yaxs="i", bty=bty)
+	}
+ 	#out <- list(...)
+  	class(out) <- "template"
+	
+	
+	if (verbose==TRUE){
+		cat(paste(" --> Journal's template selected:", journals[w],"\n"))
+		cat(" --> Plot is ready")	
+	}
+	
+	return(out)
+	
 }
 
+
+# convertor 
+
+as.template <- function(...){
+	
+}
 
 
 # application
@@ -30,6 +61,5 @@ own <- template(  pch = 20,
   
 
 par(own)
-
 
 plot(y ~ x)
